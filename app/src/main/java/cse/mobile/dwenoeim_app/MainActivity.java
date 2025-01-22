4ea96ce452f2ea54dd11b3a80977a90ad91908c6
inhooo00 <chldlsgh0987@naver.com>
2025-01-22 21:23:34 +0900

    feat: main 화면 구성

--- app/src/main/java/cse/mobile/dwenoeim_app/MainActivity.java
+++ app/src/main/java/cse/mobile/dwenoeim_app/MainActivity.java
@@ -1,6 +1,9 @@
 package cse.mobile.dwenoeim_app;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,12 +16,13 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+
+//        // Facebook 버튼 클릭 이벤트
+//        Button facebookLoginButton = findViewById(R.id.logoImageView);
+//        facebookLoginButton.setOnClickListener(v ->
+//                Toast.makeText(MainActivity.this, "kakao 로그인 클릭됨", Toast.LENGTH_SHORT).show()
+//        );
     }
 }
